4645f81c52a8d3a30a7bb7ad4d0524c4a618358e
asmafurniturewala <asmafurniturewala368@gmail.com>
2023-12-07 14:18:52 -0500

    Merged changes

---
+++ src/Pages/ForgotPassword/ForgotPassword.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './ForgotPassword.css';
+import Header from "../../Components/Header/Header.js";
+import Footer from "../../Components/Footer/Footer";
+
+function ForgotPassword() {
+  return (
+    <div className="App">
+       <Header />
+      <form className="form-forgotpassword">
+        <div className="forgotpassword_content">
+        <h1 className="forgotpassword_text">Forgot Password?</h1>
+        <input className= "forgotpassword_input" type="email" placeholder="Email" required />
+   
+        <input className="forgotpassword_submit" type="submit" value="Send Email" />
+        <a href="/">Sign In Instead</a>
+      
+        </div>
+      </form>
+      <Footer/>
+    </div>
+  );
+}
+
+export default ForgotPassword;
