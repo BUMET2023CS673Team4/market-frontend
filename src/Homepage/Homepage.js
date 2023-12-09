a256633505973db2e9677da543a29bb21bd4d962
tpcaibu <tpcai@bu.edu>
2023-12-09 21:09:05 +0000

    updated images

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -13,11 +13,12 @@ class Homepage extends react.Component{
     constructor(props){
         super(props)
         this.state={
-            position:[
+            position: [
                 {left : "27%", top : "56%"},
                 {left : "43%", top : "56%"},
                 {left : "59%", top : "56%"}
             ],
+            pics : [Eicon, Bicon, Ficon],
         }
     }
 
@@ -35,7 +36,7 @@ class Homepage extends react.Component{
                 <Header />
                 <Carousel />
                 {Object.entries(this.state.data || []).map(([idx, {id, name, image}]) => (
-                    <CategoryCard key={idx} pos={this.state.position[idx]} productName={name} productUrl={"/products/"+id} pic={Eicon} />
+                    <CategoryCard key={idx} pos={this.state.position[idx]} productName={name} productUrl={"/products/"+id} pic={this.state.pics[idx]} />
                 ))}
                 {/* <CategoryCard pos={this.state.position[0]} productName="Electronics" productUrl="/product/electronics" pic={Eicon} /> */}
                 {/* <CategoryCard pos={this.state.position[1]} productName="Textbooks" productUrl="/product/textbooks" pic={Bicon} /> */}
@@ -47,22 +48,3 @@ class Homepage extends react.Component{
 }
 
 export default Homepage;
-
-// export default function Homepage(){
-//     const position=[
-//         {left : "27%", top : "56%"},
-//         {left : "43%", top : "56%"},
-//         {left : "59%", top : "56%"}
-//     ]
-
-//     return(
-//     <>
-//         <Header />
-//         <Carousel />
-//         <CategoryCard pos={position[0]} productName="Electronics" productUrl="/product/electronics" pic={Eicon} />
-//         <CategoryCard pos={position[1]} productName="Textbooks" productUrl="/product/textbooks" pic={Bicon} />
-//         <CategoryCard pos={position[2]} productName="Furniture" productUrl="/product/funiture" pic={Ficon} />
-//         <Footer />
-//     </>
-//     )
-// }
