f9bc1daaa4bf6c69a99d6e97b46b9f2828a41c31
tpcaibu <tpcai@bu.edu>
2023-12-11 21:24:34 +0000

    Update categories images to fetch from BE update product list

--- src/Categorires/ProductList.js
+++ src/Categorires/ProductList.js
@@ -1,24 +1,53 @@
+import react from 'react';
 import "./ProductList.css"
 import Header from "../Components/Header/Header.js";
 import Footer from "../Components/Footer/Footer";
 import ProductItem from "./ProductListItem"
+import { withRouter } from "../Utility/WithRouter";
 
-export default function ProductList(){
-    const position = [
-        { left: "20%", top : "25%" },
-        { left: "20%", top : "43%" },
-        { left: "20%", top : "61%" }
+class ProductList extends react.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            position: [
+                { left: "20%", top: "25%" },
+                { left: "20%", top: "43%" },
+                { left: "20%", top: "61%" }
+            ],
+        }
+    }
 
-    ]
+    componentDidMount = async () => {
+        let category_id = this.props.match.params.id;
+        let response_json = await fetch(`/api/categories/${category_id}/items/`).then(response => response.json());
+        console.log(response_json['items']);
+        this.setState({ items: response_json['items'] });
+    }
 
-    return(
-        <>
-        <Header />
-        <ProductItem content={null} pos={position[0]} />
-        <ProductItem content={null} pos={position[1]} />
-        <ProductItem content={null} pos={position[2]} />
-        <div className="listbar"></div>
-        <Footer />
-        </>
-    )
+    render() {
+        return (
+            <>
+                <Header />
+                <div className='productItemFlexContainer'>
+                    {this.state.items ?
+                        (this.state.items.map((item, idx) => {
+                            console.log(item)
+                            return <ProductItem imgsrc={'/media/image/'+item['media_image']} itemId={item['id']} itemName={item['name']} />
+                        })) :
+                        (
+                            <>
+                                <ProductItem />
+                                <ProductItem />
+                                <ProductItem />
+                            </>
+                        )
+                    }
+                </div>
+                <div className="listbar"></div>
+                <Footer />
+            </>
+        )
+    }
 }
+
+export default withRouter(ProductList);
