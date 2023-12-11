29d8dbfbaa78623e9e194178b844643b249e0e4b
tpcaibu <tpcai@bu.edu>
2023-12-11 08:28:24 +0000

    add support for images, change url

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -9,38 +9,38 @@ import Eicon from "./asserts/Earphone.png"
 import Bicon from "./asserts/Book.png"
 import Ficon from "./asserts/Furniture.png"
 
-class Homepage extends react.Component{
-    constructor(props){
+class Homepage extends react.Component {
+    constructor(props) {
         super(props)
-        this.state={
+        this.state = {
             position: [
-                {left : "27%", top : "56%"},
-                {left : "43%", top : "56%"},
-                {left : "59%", top : "56%"}
+                { left: "27%", top: "56%" },
+                { left: "43%", top: "56%" },
+                { left: "59%", top: "56%" }
             ],
-            pics : [Eicon, Bicon, Ficon],
+            pics: [Eicon, Bicon, Ficon],
         }
     }
 
     componentDidMount = async () => {
-        await fetch("/api/homepage/")
-        .then(response => response.json())
-        .then(data => {
-            this.setState({data: data});
-        });
+        let response_json = await fetch("/api/homepage/").then(response => response.json());
+        this.setState({ data: response_json });
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <>
                 <Header />
                 <Carousel />
-                {Object.entries(this.state.data || []).map(([idx, {id, name, image}]) => (
-                    <CategoryCard key={idx} pos={this.state.position[idx]} productName={name} productUrl={"/products/"+id} pic={this.state.pics[idx]} />
-                ))}
-                {/* <CategoryCard pos={this.state.position[0]} productName="Electronics" productUrl="/product/electronics" pic={Eicon} /> */}
-                {/* <CategoryCard pos={this.state.position[1]} productName="Textbooks" productUrl="/product/textbooks" pic={Bicon} /> */}
-                {/* <CategoryCard pos={this.state.position[2]} productName="Furniture" productUrl="/product/funiture" pic={Ficon} /> */}
+                {this.state.data ?
+                    (Object.entries(this.state.data).map(([idx, { id, name, media_image }]) => (
+                        <CategoryCard key={idx} pos={this.state.position[idx]} productName={name} productUrl={"/categories/" + id} pic={"/media/image/" + media_image} />
+                    ))) :
+                    (<>
+                        <CategoryCard pos={this.state.position[0]} productName="Electronics" productUrl="/product/electronics" pic={Eicon} />,
+                        <CategoryCard pos={this.state.position[1]} productName="Textbooks" productUrl="/product/textbooks" pic={Bicon} />,
+                        <CategoryCard pos={this.state.position[2]} productName="Furniture" productUrl="/product/funiture" pic={Ficon} />,
+                    </>)}
                 <Footer />
             </>
         )
